dfb48048d855716c6c6acc3bc57f1c09f84c16a5
James Tien <southp0105@gmail.com>
2015-11-08 14:08:38 +0800

    Use a map to store loop beg/end pair relations. Now it works!

--- src/interpreter.cpp
+++ src/interpreter.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <stack>
 #include <array>
+#include <map>
 
 #include <cassert>
 #include <cstdlib>
@@ -20,6 +21,15 @@ enum class Command {
     LoopEnd
 };
 
+struct Program {
+    Program( std::vector< Command > cmds, std::map< size_t, size_t > loop_scopes ) :
+        cmds( cmds ),
+        loop_scopes( loop_scopes ) {}
+
+    std::vector< Command > cmds;
+    std::map< size_t, size_t > loop_scopes;
+};
+
 class Environment {
 public:
     Environment( std::istream& in = std::cin, std::ostream& out = std::cout ) :
@@ -31,19 +41,21 @@ public:
     }
 
     // Well, there could be richer status code.
-    auto execute( const std::vector< Command >& cmds ) -> int {
-        auto loop_stack = std::stack< int >();
-        size_t total_cmds = cmds.size();
+    auto execute( const Program& prog ) -> int {
+        size_t total_cmds = prog.cmds.size();
 
         for( size_t inst_ptr = 0; inst_ptr < total_cmds; ) {
-            inst_ptr = execute_command( cmds, inst_ptr, loop_stack );
+            inst_ptr = execute_command( prog, inst_ptr );
         }
 
         return 0;
     }
 
 private:
-    auto execute_command( std::vector< Command > cmds, size_t inst_ptr, std::stack< int >& loop_stack ) -> size_t {
+    auto execute_command( const Program& prog, size_t inst_ptr ) -> size_t {
+        auto& cmds = prog.cmds;
+        auto& loop_scopes = prog.loop_scopes;
+
         assert( data_ptr >= 0 && data_ptr < DATA_BUFFER_SIZE );
         assert( inst_ptr >= 0 && inst_ptr < cmds.size() );
 
@@ -81,19 +93,23 @@ private:
             }
 
             case Command::LoopBegin: {
-                loop_stack.push( inst_ptr );
-                return inst_ptr + 1;
+                if ( 0 == data_buffer[ data_ptr ] ) {
+                    auto loop_end = loop_scopes.find( inst_ptr );
+
+                    assert( loop_end != loop_scopes.end() );
+
+                    return loop_end->second + 1;
+                } else {
+                    return inst_ptr + 1;
+                }
             }
 
             case Command::LoopEnd: {
-                if ( 0 == data_buffer[ data_ptr ] ) {
-                    loop_stack.pop();
-                    return inst_ptr + 1;
+                auto loop_beg = loop_scopes.find( inst_ptr );
 
-                } else {
-                    int loop_beg = loop_stack.top();
-                    return loop_beg + 1;
-                }
+                assert( loop_beg != loop_scopes.end() );
+
+                return loop_beg->second;
             }
 
             default: {
@@ -129,8 +145,10 @@ private:
     std::ostream& out_device;
 };
 
-auto parse( const std::string& command_str) -> std::vector< Command > {
+auto parse( const std::string& command_str ) -> Program {
     auto commands = std::vector< Command >();
+    auto loop_scopes = std::map< size_t, size_t >();
+    auto loop_stack  = std::stack< size_t >();
 
     for( auto c : command_str ) {
         switch( c ) {
@@ -166,11 +184,22 @@ auto parse( const std::string& command_str) -> std::vector< Command > {
 
             case '[': {
                 commands.push_back( Command::LoopBegin );
+                loop_stack.push( commands.size() - 1 );
+
                 break;
             }
 
             case ']': {
                 commands.push_back( Command::LoopEnd );
+
+                auto loop_beg_idx = loop_stack.top();
+                auto cur_cmd_idx  = commands.size() - 1;
+
+                loop_scopes[ cur_cmd_idx  ] = loop_beg_idx;
+                loop_scopes[ loop_beg_idx ] = cur_cmd_idx;
+
+                loop_stack.pop();
+
                 break;
             }
 
@@ -179,7 +208,7 @@ auto parse( const std::string& command_str) -> std::vector< Command > {
         }
     }
 
-    return commands;
+    return Program( commands, loop_scopes );
 }
 
 // TODO: could make use of streaming to reduce memory footprint
@@ -210,7 +239,7 @@ int main( int argc, char* argv[] ) {
 
     auto env        = Environment();
     auto src_string = read_source( src );
-    auto cmds       = parse( src_string );
+    auto program    = parse( src_string );
 
-    return env.execute( cmds );
+    return env.execute( program );
 }
